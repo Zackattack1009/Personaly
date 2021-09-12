12b1a41a5c07b281b9e3767601104510c5b90460
Zackattack1009 <73863307+Zackattack1009@users.noreply.github.com>
2021-09-11 23:49:10 -0400

    Update fix.bat

--- fix.bat
+++ fix.bat
@@ -1,20 +1,3 @@
 @echo off
-:start
-title Minecraft client fix
-echo do you want to fix this?(y/n)
-set/p "fix=> "
-if %fix%==y goto y
-if %fix%==n goto n
-cls
-title Unknown option
-goto start
-
-:y
-cd %appdata%\.minecraft
-del usercache.json
-del usernamecache.json
-del options.txt
+echo hi you gay
 pause
-
-:n
-exit
