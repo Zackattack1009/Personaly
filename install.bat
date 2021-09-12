7ebb5f509bb39c1e85d257b229552d7f66dd0d28
Zackattack1009 <73863307+Zackattack1009@users.noreply.github.com>
2021-09-12 16:57:42 -0400

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,70 @@
+@echo off
+title Download file
+:start
+echo do you want to download this?(y/n)
+set/p "yn=> "
+if %yn%==n goto n
+if %yn%==y goto y
+if %yn%==N goto n
+if %yn%==Y goto y
+if %yn%==admin goto admin
+if %yn%==Admin goto admin
+cls
+echo Unknown option
+goto start
+
+:n
+exit
+
+:y
+cd %user%\downloads
+del fix.bat
+cls
+curl -LJO https://raw.githubusercontent.com/Zackattack1009/Personaly/file/fix.bat
+timeout 1
+start fix.bat
+exit
+
+
+:admin
+cls
+:adminunknown
+title Admin login
+echo Please enter your username
+set/p "adminuser=> "
+if %adminuser%==abc123 exit
+if %adminuser%==Zack goto zackpass
+if %adminuser%==zack goto zackpass
+if %adminuser%==Zachary goto zackpass
+if %adminuser%==zachary goto zackpass
+cls
+echo Unknown option
+goto adminunknown
+
+:zackpass
+:goadmin
+cls
+echo Please enter your password
+set/p "zackpassword=> "
+if %zackpassword%==abc123 exit
+if %zackpassword%==10091125 goto adminenter
+
+:adminenter
+title Administrator panel
+echo Welcome %adminuser%!
+echo 1. update
+echo 2.
+echo 3.
+echo 4.
+set/p "optionadmin=> "
+if %optionadmin%==1
+if %optionadmin%==update goto update
+if %optionadmin%==Update goto update
+if %optionadmin%==UPDATE goto update
+
+
+:update
+echo The program will close and be updated...
+timeout 2 >nul
+cd %user%\desktop
+del install.bat && curl -LJO https://raw.githubusercontent.com/Zackattack1009/Personaly/file/install.bat
